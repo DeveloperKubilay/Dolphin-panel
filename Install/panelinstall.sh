16b24a3e8f8feffc38fb21927acf9a30e3aa144b
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2023-02-11 04:50:08 +0300

    Update panelinstall.sh

--- Install/panelinstall.sh
+++ Install/panelinstall.sh
@@ -1,7 +1,6 @@
 echo "------------------------------------"
 echo "   WELCOME TO DOLPHIN PANEL SETUP   "
 echo "------------------------------------"
-echo "Do you want to install nodejs? Y/N"
 echo "Installing nodejs@16"
 sudo apt update -y
 curl -s https://deb.nodesource.com/setup_16.x | sudo bash
@@ -14,5 +13,4 @@ clear
 echo ------------------------------------
 echo The installation is complete, you can go to the main directory and run npm start
 echo ------------------------------------
-cd ..
 npm start
